3b83438b09dae69cd5ca1c0a5350e118b43f51d3
Can Korkut <33387838+CanKorkut@users.noreply.github.com>
2024-10-20 17:58:17 +0300

    Delete init_model.py

--- init_model.py
+++
@@ -1,6 +0,0 @@
-from transformers import T5ForConditionalGeneration, T5Tokenizer
-
-def load_t5_model():
-    tokenizer = T5Tokenizer.from_pretrained('t5-small')
-    t5_model = T5ForConditionalGeneration.from_pretrained('t5-small')
-    return tokenizer, t5_model
